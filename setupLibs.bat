c4c80bf06ab05b17ef4bbf40bb46f05bf39d54bf
One5hot76 <ftyre76@gmail.com>
2022-01-08 15:23:18 -0600

    Initial Creation of Repository
    Writer Sprints Organizer Bot

---
+++ setupLibs.bat
@@ -0,0 +1,9 @@
+J:
+cd J:\One5hot76\softwareDev\slackBots
+PATH=C:\Python39\Scripts\;C:\Python39\;C:\Users\One5hot76\AppData\Roaming\Python\Python39\Scripts;%PATH%
+python -m pip install --upgrade pip --user
+pip install slackclient
+pip install python-dotenv
+pip install flask
+pip install slackeventsapi
+pause
---
+++ writer-sprints-organizer-bot.py
@@ -0,0 +1,18 @@
+import slack
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+from slackeventsapi import SlackEventAdapter
+
+env_path = '.' / '.env'
+load_dotenv(dotenv_path = env_path)
+
+app = Flask(__name__)
+slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'], '/slack/events', app)
+
+client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
+
+client.chat_postMessage(channel='#test', text="Hello!")
+
+if __name__ == "__main__":
+    app.run(debug=True)
